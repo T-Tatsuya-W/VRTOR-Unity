57d05c321bac665558a256b545329f01e79d81ba
Toby T Watkinson <tobytw312@gmail.com>
2026-05-09 00:09:06 +0100

    basic unity stuff

---
+++ unity/MusicalDonuts/Assets/Scripts/ColourCubeRed.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ColourCubeRed : MonoBehaviour
+{
+    private Keyboard keyboard;
+    void Awake()
+    {
+        keyboard = Keyboard.current;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (keyboard != null && keyboard.spaceKey.wasPressedThisFrame)
+        {
+            Debug.Log("Space key was pressed.");
+            
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (keyboard != null && keyboard.spaceKey.wasReleasedThisFrame)
+        {
+            Debug.Log("Space key was released.");
+            
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
---
+++ unity/MusicalDonuts/Assets/Scripts/TorusMesh.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class TorusMesh : MonoBehaviour
+{
+    private Keyboard keyboard;
+    void Awake()
+    {
+        keyboard = Keyboard.current;
+    }
+    public float majRadius = 1f;
+    public float minorRadius = 0.5f;
+    public int numSegments = 16;
+
+    void Start()
+    {
+        GenerateTorus();
+    }
+
+    void Update()
+    {
+        // if the radiu or segments are changed, regenerate torus
+        if (keyboard != null && keyboard.spaceKey.wasPressedThisFrame){ 
+            
+            GenerateTorus();
+        }
+    }
+
+    void GenerateTorus()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        int radialSegments = numSegments;
+        int tubularSegments = numSegments;
+
+        int numVertices = (radialSegments + 1) * (tubularSegments + 1);
+        Vector3[] vertices = new Vector3[numVertices];
+        Vector3[] normals = new Vector3[numVertices];
+        Vector2[] uvs = new Vector2[numVertices];
+        int[] triangles = new int[radialSegments * tubularSegments * 6];
+
+        int vertexIndex = 0;
+        for (int i = 0; i <= radialSegments; i++)
+        {
+            float theta = (float)i / radialSegments * 2f * Mathf.PI;
+            for (int j = 0; j <= tubularSegments; j++)
+            {
+                float phi = (float)j / tubularSegments * 2f * Mathf.PI;
+
+                float x = (majRadius + minorRadius * Mathf.Cos(theta)) * Mathf.Cos(phi);
+                float y = (majRadius + minorRadius * Mathf.Cos(theta)) * Mathf.Sin(phi);
+                float z = minorRadius * Mathf.Sin(theta);
+
+                vertices[vertexIndex] = new Vector3(x, y, z);
+
+                // Normal calculation
+                Vector3 center = new Vector3(majRadius * Mathf.Cos(phi), majRadius * Mathf.Sin(phi), 0f);
+                normals[vertexIndex] = (vertices[vertexIndex] - center).normalized;
+
+                uvs[vertexIndex] = new Vector2((float)j / tubularSegments, (float)i / radialSegments);
+
+                vertexIndex++;
+            }
+        }
+
+        int triangleIndex = 0;
+        for (int i = 0; i < radialSegments; i++)
+        {
+            for (int j = 0; j < tubularSegments; j++)
+            {
+                int a = i * (tubularSegments + 1) + j;
+                int b = i * (tubularSegments + 1) + j + 1;
+                int c = (i + 1) * (tubularSegments + 1) + j;
+                int d = (i + 1) * (tubularSegments + 1) + j + 1;
+
+                triangles[triangleIndex++] = a;
+                triangles[triangleIndex++] = c;
+                triangles[triangleIndex++] = b;
+
+                triangles[triangleIndex++] = b;
+                triangles[triangleIndex++] = c;
+                triangles[triangleIndex++] = d;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateBounds();
+    }
+}
